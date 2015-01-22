6e10336b39cf95fb630a4ee615a90cffd4892440
Mark Dietzer <mriq91@gmail.com>
2015-01-22 04:03:56 -0800

    Move the entire system around

--- src/mount.cc
+++ src/mount.cc
@@ -8,52 +8,16 @@
 v8::Handle<v8::Value> Mount(const v8::Arguments &args) {
   v8::HandleScope scope;
 
-  if (args.Length() < 3) {
+  if (args.Length() != 5) {
     // TODO: make it raise a proper exception
-    return v8::ThrowException(v8::String::New("`mount` needs at least 3 parameters"));
-  }
-
-  bool callback = false;
-  v8::Local<v8::Function> cb;
-  v8::Handle<v8::Array> options;
-  v8::Local<v8::String> data;
-
-  if(args.Length() == 4) {
-    callback = true;
-    cb = v8::Local<v8::Function>::Cast(args[3]);
-    options = v8::Array::New(0);
-  } else if(args.Length() == 5) {
-    callback = true;
-    cb = v8::Local<v8::Function>::Cast(args[4]);
-    if (args[3]->IsArray()) {
-      options = v8::Handle<v8::Array>::Cast(args[3]);
-    }
-  } else if(args.Length() == 6) {
-    callback = true;
-    cb = v8::Local<v8::Function>::Cast(args[5]);
-    if (args[3]->IsArray()) {
-      options = v8::Handle<v8::Array>::Cast(args[3]);
-    }
-    if (args[4]->IsString()) {
-      data = v8::Local<v8::String>::Cast(args[4]);
-    }
-  }
-
-  int mask = 0;
-  for (unsigned int i = 0; i < options->Length(); i++) {
-    v8::Local<v8::String> opt = v8::Local<v8::String>::Cast(options->Get(i));
-    if(opt == v8::String::New("bind")) {
-      mask |= MS_BIND;
-    } else if(opt == v8::String::New("readonly")) {
-      mask |= MS_RDONLY;
-    } else if(opt == v8::String::New("remount")) {
-      mask |= MS_REMOUNT;
-    }
+    return v8::ThrowException(v8::String::New("`mount` needs 5 parameters"));
   }
 
   v8::String::Utf8Value device(args[0]->ToString());
   v8::String::Utf8Value path(args[1]->ToString());
   v8::String::Utf8Value type(args[2]->ToString());
+  int mask = args[3]->ToInt32()->Value();
+  v8::String::Utf8Value data(args[4]->ToString());
 
   int mountNum = mount(*device, *path, *type, mask, *data);
   
@@ -61,27 +25,14 @@ v8::Handle<v8::Value> Mount(const v8::Arguments &args) {
 
   v8::Local<v8::Value> mounted = v8::Local<v8::Value>::New(v8::Boolean::New(mountAction));
 
-  if(callback) {
-    const unsigned argc = 1;
-    v8::Local<v8::Value> argv[argc] = {mounted};
-    cb->Call(v8::Context::GetCurrent()->Global(), argc, argv);
-  }
-  return mounted;
+  return scope.Close(mounted);
 }
 
 v8::Handle<v8::Value> Unmount(const v8::Arguments &args) {
   v8::HandleScope scope;
 
-  if (args.Length() < 1) {
-    return v8::ThrowException(v8::String::New("`umount` needs at least 1 parameter"));
-  }
-
-  bool callback = false;
-  v8::Local<v8::Function> cb;
-  
-  if(args.Length() == 2) {
-    callback = true;
-    cb = v8::Local<v8::Function>::Cast(args[1]);
+  if (args.Length() != 1) {
+    return v8::ThrowException(v8::String::New("`umount` needs 1 parameter"));
   }
 
   v8::String::Utf8Value path(args[0]->ToString());
@@ -90,17 +41,28 @@ v8::Handle<v8::Value> Unmount(const v8::Arguments &args) {
 
   v8::Local<v8::Value> mounted = v8::Local<v8::Value>::New(v8::Boolean::New(mountAction));
 
-  if(callback) {
-    const unsigned argc = 1;
-    v8::Local<v8::Value> argv[argc] = {mounted};
-    cb->Call(v8::Context::GetCurrent()->Global(), argc, argv);
-  }
-  return mounted;
+  return scope.Close(mounted);
 }
+
+#define EXPORT_CONST(__NAME__) exports->Set(v8::String::NewSymbol(#__NAME__), v8::Int32::New(__NAME__), v8::ReadOnly)
 
 void init (v8::Handle<v8::Object> exports, v8::Handle<v8::Object> module) {
   exports->Set(v8::String::NewSymbol("mount"), v8::FunctionTemplate::New(Mount)->GetFunction());
   exports->Set(v8::String::NewSymbol("unmount"), v8::FunctionTemplate::New(Unmount)->GetFunction());
+  
+  EXPORT_CONST(MS_BIND);
+  EXPORT_CONST(MS_MOVE);
+  
+  EXPORT_CONST(MS_NOATIME);
+  EXPORT_CONST(MS_NODIRATIME);
+  EXPORT_CONST(MS_RELATIME);
+  
+  EXPORT_CONST(MS_NODEV);
+  EXPORT_CONST(MS_NOEXEC);
+  EXPORT_CONST(MS_NOSUID);
+  
+  EXPORT_CONST(MS_RDONLY);
+  EXPORT_CONST(MS_REMOUNT);
 }
 NODE_MODULE(mount, init)
 
